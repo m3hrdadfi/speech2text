12966c0d2841c22485ccadde7137c58bf19edc90
Mehrdad Farahani <m3hrdadfi@gmail.com>
2019-04-23 01:50:07 +0430

    Support audio wav file

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from mic import MicroPhoneReader
+from audio import AudioReader
+import random
 
 
 def mic_example():
@@ -8,5 +10,15 @@ def mic_example():
         print(text)
 
 
+def audio_example():
+    audio = AudioReader(audio_file='sample.wav', language='fa-IR', verbose=True)
+    status, text = audio.read()
+    if status:
+        print(text)
+
+
 if __name__ == '__main__':
-    mic_example()
+    examples = [mic_example, audio_example]
+    idx = random.randint(0, len(examples) - 1)
+    example = examples[idx]
+    example()
--- mic.py
+++ mic.py
@@ -1,60 +1,22 @@
 import speech_recognition as sr
+from reader import Reader
 
 
-class MicroPhoneReader:
+class MicroPhoneReader(Reader):
     """A class based on the speech recognition package to listen to audio continuously via microphone inputs."""
 
-    UnknownValueError = {
-        "en-US": "Google Speech Recognition could not understand audio",
-        "fa-IR": "تشخیص گفتار گوگل نتوانست صدا را درک کند."
-    }
-    RequestError = {
-        "en-US": "Could not request results from Google Speech Recognition service: {0}",
-        "fa-IR": "درخواست نتایج از سرویس شناسایی گفتار گوگل امکان پذیر نیست: {0}"
-    }
-    ADJUST_AMB_NOISE = {
-        'en-US': "Adjusting microphone.",
-        'fa-IR': "تنظیم میکروفن."
-    }
-    ENERGY_THRESHOLD = {
-        "en-US": "Set the minimum energy threshold to {0}.",
-        "fa-IR": "حداقل آستانه انرژی را به {0} تنظیم کنید."
-    }
     RECORD = {
         "en-US": "Say something!",
         "fa-IR": "چیزی بگو!"
     }
 
     def __init__(self, language='fa-IR', verbose=False):
-        self.sr = sr
-        self.r = sr.Recognizer()
-        self.language = language
-        self.verbose = verbose
-
-    @staticmethod
-    def _print(message, verbose=False):
-        if verbose:
-            print(message)
+        super(MicroPhoneReader, self).__init__(language, verbose)
 
     def read(self):
         self.adjust_amb_noise(self.language)
         audio = self.record(self.language)
         return self.recognize_google(audio, self.language)
-
-    def recognize_google(self, audio, language):
-
-        try:
-            text = self.r.recognize_google(audio, language=language)
-        except self.sr.UnknownValueError:
-            err = self.UnknownValueError.get(language, '')
-            self._print(err, self.verbose)
-            return False, err
-        except self.sr.RequestError as e:
-            err = self.RequestError.get(language, '').format(e)
-            self._print(err, self.verbose)
-            return False, err
-
-        return True, text
 
     def adjust_amb_noise(self, language):
         self._print(self.ADJUST_AMB_NOISE.get(language, ''), self.verbose)
